17ae87bbe40e9620c8536ab1a04d3831ec49fa46
marcospeterson <marcospetersonmartins@gmail.com>
2026-04-30 11:50:22 -0300

    Exercicio 1 de vetor

---
+++ AULA_04_Vetores/exercicio_01.c
@@ -0,0 +1,26 @@
+/* #include <stdio.h>
+
+void exibir_vetor(int vetor[], int n){
+    for (int i=0; i < n; i++){
+    int valor = vetor[i];
+    printf ("O indice do valor e: %d\nO valor do vetor e: %d\n\n", i, valor);
+    }
+}
+
+ int main(){
+    int qtd;
+    printf("Digite a quantidade de notas que gostaria: ");
+    scanf("%d", &qtd);
+    float notas[qtd];
+
+    for (int i=0; i <qtd; i++){
+        printf("Digite a nota %d: ", i + 1);
+        scanf("%f", &notas[i]);
+        float soma += notas[i];
+    }
+
+    printf("A media do aluno e: %.2f\n", soma/qtd);
+    return 0;
+ } */
+
+ //incompleto
